7089822e71a46f43ac35519196d39386dd2c5872
Rolands Priednieks <rolands.priednieks@gmail.com>
2021-01-17 13:55:27 +0200

    gitignore file

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => console.log(`Server running at :${PORT}`));
--- public/js/myScript.js
+++ public/js/myScript.js
@@ -22,10 +22,9 @@ function loadImage() {
 }
 
 function imageIsLoaded(x) {
-  if (x == null || ! x.complete()) {
-  alert("Picture is not loaded");
-  }
-  else {
+  if (x == null || !x.complete()) {
+    alert("Picture is not loaded");
+  } else {
     return true;
   }
 }
@@ -39,7 +38,7 @@ function doGray() {
 
 function filterGray() {
   for (var pixel of image1.values()) {
-    var avg = (pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3;
+    var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
     pixel.setRed(avg);
     pixel.setGreen(avg);
     pixel.setBlue(avg);
@@ -56,16 +55,15 @@ function doRed() {
 
 function filterRed() {
   for (var pixel of image2.values()) {
-    var avg = (pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3;
+    var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
     if (avg < 128) {
-      pixel.setRed(avg*2);
+      pixel.setRed(avg * 2);
       pixel.setGreen(0);
       pixel.setBlue(0);
-    }
-    else {
+    } else {
       pixel.setRed(255);
-      pixel.setGreen(avg*2-255);
-      pixel.setBlue(avg*2-255);
+      pixel.setGreen(avg * 2 - 255);
+      pixel.setBlue(avg * 2 - 255);
     }
   }
 }
@@ -78,19 +76,17 @@ function doColors() {
 }
 
 function filterColors() {
-  for (var pixel of image3.values()){
+  for (var pixel of image3.values()) {
     x = pixel.getX();
     var width = image3.getWidth();
     var pirma = width / 3;
-    var tresa = width / 3*2;
-    if (x < pirma){
-        pixel.setRed(255);
-    }
-    else if (x > tresa){
-        pixel.setBlue(255);
-    }
-    else {
-        pixel.setGreen(255);
+    var tresa = (width / 3) * 2;
+    if (x < pirma) {
+      pixel.setRed(255);
+    } else if (x > tresa) {
+      pixel.setBlue(255);
+    } else {
+      pixel.setGreen(255);
     }
   }
 }
@@ -116,90 +112,83 @@ function doRainbow() {
 
 function filterRainbow() {
   var height = image4.getHeight();
-  var a = height/7;
+  var a = height / 7;
   for (var pixel of image4.values()) {
     var y = pixel.getY();
-    var avg = (pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3;
-    
+    var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+
     if (y <= a) {
-    if (avg < 128) {
-      pixel.setRed(avg*2);
-      pixel.setGreen(0);
-      pixel.setBlue(0);
-    }
-    else {
-      pixel.setRed(255);
-      pixel.setGreen(avg*2 - 255);
-      pixel.setBlue(avg*2 - 255);
-    }
-    } else if (y <= a*2) {
-    if (avg < 128) {
-      pixel.setRed(avg*2);
-      pixel.setGreen(0.8*avg);
-      pixel.setBlue(0);
-    }
-    else {
-      pixel.setRed(255);
-      pixel.setGreen(avg*1.2 - 51);
-      pixel.setBlue(avg*2 - 255);
-    }
-    } else if(y <= a*3) {
-    if (avg < 128) {
-      pixel.setRed(avg*2);
-      pixel.setGreen(avg*2);
-      pixel.setBlue(0);
-    }
-    else {
-      pixel.setRed(255);
-      pixel.setGreen(255);
-      pixel.setBlue(avg*2 - 255);
-    }
-    } else if(y <= a*4) {
-    if (avg < 128) {
-      pixel.setRed(0);
-      pixel.setGreen(avg*2);
-      pixel.setBlue(0);
-    }
-    else {
-      pixel.setRed(avg*2 - 255);
-      pixel.setGreen(255);
-      pixel.setBlue(avg*2 - 255);
-    }
-    } else if(y <= a*5) {
-    if (avg < 128) {
-      pixel.setRed(0);
-      pixel.setGreen(0);
-      pixel.setBlue(avg*2);
-    }
-    else {
-      pixel.setRed(avg*2 - 255);
-      pixel.setGreen(avg*2 - 255);
-      pixel.setBlue(255);
-    }
-    } else if(y <= a*6) {
-    if (avg < 128) {
-      pixel.setRed(avg*0.8);
-      pixel.setGreen(0);
-      pixel.setBlue(avg*2);
-    }
-    else {
-      pixel.setRed(1.2*avg - 51);
-      pixel.setGreen(avg*2 - 255);
-      pixel.setBlue(255);
-    }
+      if (avg < 128) {
+        pixel.setRed(avg * 2);
+        pixel.setGreen(0);
+        pixel.setBlue(0);
+      } else {
+        pixel.setRed(255);
+        pixel.setGreen(avg * 2 - 255);
+        pixel.setBlue(avg * 2 - 255);
+      }
+    } else if (y <= a * 2) {
+      if (avg < 128) {
+        pixel.setRed(avg * 2);
+        pixel.setGreen(0.8 * avg);
+        pixel.setBlue(0);
+      } else {
+        pixel.setRed(255);
+        pixel.setGreen(avg * 1.2 - 51);
+        pixel.setBlue(avg * 2 - 255);
+      }
+    } else if (y <= a * 3) {
+      if (avg < 128) {
+        pixel.setRed(avg * 2);
+        pixel.setGreen(avg * 2);
+        pixel.setBlue(0);
+      } else {
+        pixel.setRed(255);
+        pixel.setGreen(255);
+        pixel.setBlue(avg * 2 - 255);
+      }
+    } else if (y <= a * 4) {
+      if (avg < 128) {
+        pixel.setRed(0);
+        pixel.setGreen(avg * 2);
+        pixel.setBlue(0);
+      } else {
+        pixel.setRed(avg * 2 - 255);
+        pixel.setGreen(255);
+        pixel.setBlue(avg * 2 - 255);
+      }
+    } else if (y <= a * 5) {
+      if (avg < 128) {
+        pixel.setRed(0);
+        pixel.setGreen(0);
+        pixel.setBlue(avg * 2);
+      } else {
+        pixel.setRed(avg * 2 - 255);
+        pixel.setGreen(avg * 2 - 255);
+        pixel.setBlue(255);
+      }
+    } else if (y <= a * 6) {
+      if (avg < 128) {
+        pixel.setRed(avg * 0.8);
+        pixel.setGreen(0);
+        pixel.setBlue(avg * 2);
+      } else {
+        pixel.setRed(1.2 * avg - 51);
+        pixel.setGreen(avg * 2 - 255);
+        pixel.setBlue(255);
+      }
     } else {
-    if (avg < 128) {
-      pixel.setRed(avg*1.6);
-      pixel.setGreen(0);
-      pixel.setBlue(avg*1.6);
+      if (avg < 128) {
+        pixel.setRed(avg * 1.6);
+        pixel.setGreen(0);
+        pixel.setBlue(avg * 1.6);
+      } else {
+        pixel.setRed(0.4 * avg + 153);
+        pixel.setGreen(avg * 2 - 255);
+        pixel.setBlue(0.4 * avg + 153);
+      }
     }
-    else {
-      pixel.setRed(0.4*avg + 153);
-      pixel.setGreen(avg*2 - 255);
-      pixel.setBlue(0.4*avg + 153);
-    }
-    }
-   }
+  }
 }
 
 function doPicking() {
@@ -212,21 +201,19 @@ function doPicking() {
 
 function filterPicking() {
   var picker = document.getElementById("htmlPicker");
-  var rc = parseInt("0x"+picker.value.slice(1,3));
-  var gc = parseInt("0x"+picker.value.slice(3,5));
-  var bc = parseInt("0x"+picker.value.slice(5,7));
+  var rc = parseInt("0x" + picker.value.slice(1, 3));
+  var gc = parseInt("0x" + picker.value.slice(3, 5));
+  var bc = parseInt("0x" + picker.value.slice(5, 7));
   for (var pixel of image5.values()) {
-    var avg = (pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3;
-  if (avg < 128) {
-      pixel.setRed(rc/127.5*avg);
-      pixel.setGreen(gc/127.5*avg);
-      pixel.setBlue(bc/127.5*avg);
-    }
-    else {
-      pixel.setRed((2 - rc/127.5)*avg + 2*rc - 255);
-      pixel.setGreen((2 - gc/127.5)*avg + 2*gc - 255);
-      pixel.setBlue((2 - bc/127.5)*avg + 2*bc - 255);
+    var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+    if (avg < 128) {
+      pixel.setRed((rc / 127.5) * avg);
+      pixel.setGreen((gc / 127.5) * avg);
+      pixel.setBlue((bc / 127.5) * avg);
+    } else {
+      pixel.setRed((2 - rc / 127.5) * avg + 2 * rc - 255);
+      pixel.setGreen((2 - gc / 127.5) * avg + 2 * gc - 255);
+      pixel.setBlue((2 - bc / 127.5) * avg + 2 * bc - 255);
     }
   }
 }
-
